808ec3dee07a540af00b4c5b5313671c432f754d
Surajprakash Ankush <surajankush@gmail.com>
2021-06-13 23:20:30 +0530

    start.bat

--- start.bat
+++ start.bat
@@ -1,1 +1,4 @@
-start-process dotnet run -NoNewWindow -PassThru
+cd C:\Users\Administrator\Downloads\Pkg
+C:\Program Files\dotnet\dotnet publish
+cd bin\Debug\net5.0
+powershell start-process myWebApp.exe -WindowStyle Hidden
