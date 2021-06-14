39c9e0f3774099709800b4a578d32a6353a58b7a
surajankush <surajankush@gmail.com>
2021-06-14 22:34:19 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,6 @@
 powershell stop-process -name myWebApp
 RMDIR C:\Users\Administrator\Downloads\Pkg /S /Q
+MKDIR C:\Users\Administrator\Downloads\Pkg
 xcopy /s C:\Users\Administrator\Downloads\Pre_Pkg C:\Users\Administrator\Downloads\Pkg 
 cd C:\Users\Administrator\Downloads\Pkg\publish
 powershell start-process myWebApp.exe -WindowStyle Hidden
