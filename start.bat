8eb21fecfc9ded0e76d0c97cb24cd05d51ccf297
surajankush <surajankush@gmail.com>
2021-06-14 22:46:41 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,5 +4,5 @@ MKDIR C:\Users\Administrator\Downloads\Pkg
 xcopy /s C:\Users\Administrator\Downloads\Pre_Pkg C:\Users\Administrator\Downloads\Pkg 
 cd C:\Users\Administrator\Downloads\Pkg\publish
 powershell start-process myWebApp.exe -WindowStyle Hidden
-RMDIR C:\Users\Administrator\Downloads\Pre_Pkg
+RMDIR C:\Users\Administrator\Downloads\Pre_Pkg /S /Q
 MKDIR C:\Users\Administrator\Downloads\Pre_Pkg
